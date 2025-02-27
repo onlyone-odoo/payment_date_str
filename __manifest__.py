6d522e65bad7caacaefe781bfa54c484cab693ad
onlyone-odoo <matiasb@onlyone.com.ar>
2025-02-27 10:42:21 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment Day String",
+    "summary": """
+        Adds a sting to account.move tree view to show the dates of any payments asociated with invoices""",
+    "author": "Be OnlyOne",
+    "maintainers": ["onlyone-odoo"],
+    "website": "https://onlyone.odoo.com/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "17.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["base", "account"],
+    "data": [
+        "views/account_move_tree.xml",
+    ],
+}
---
+++ models/account_move.py
@@ -0,0 +1,29 @@
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    payment_date_str = fields.Char(
+        string="Fechas de Pago",
+        compute="_compute_payment_date_str",
+        store=True,
+        help="Fechas de los pagos asociados a esta factura, separadas por coma.",
+    )
+
+    @api.depends("payment_ids", "payment_ids.date", "type")
+    def _compute_payment_date_str(self):
+        for move in self:
+            payments = move.payment_ids.filtered(lambda p: p.state == "posted")
+            if payments:
+                payment_dates = sorted(payments.mapped("date"))
+                move.payment_date_str = ", ".join(
+                    date.strftime("%Y-%m-%d") for date in payment_dates
+                )
+            else:
+                move.payment_date_str = False
+            # Opcional: ajustar el nombre del campo según el tipo
+            if move.type == "out_invoice":
+                move.payment_date_str = move.payment_date_str or "Sin pagos recibidos"
+            elif move.type == "in_invoice":
+                move.payment_date_str = move.payment_date_str or "Sin pagos realizados"
